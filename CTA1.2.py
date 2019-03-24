8f1f9e929a38f51b6fcc1435dbd513591345e10b
JessH01 <jessica.ham@csuglobal.edu>
2019-03-24 14:30:59 -0500

    Edit Comments on script

--- CTA1.2.py
+++ CTA1.2.py
@@ -1,7 +1,7 @@
 SALARY_RAISE_FACTOR = 0.05
 STATE_CODE_MAP = {'WA': 'Washington', 'TX': 'Texas'}
 
-# This is the beginning of the script
+# This is the definition of the class
 
 class Salary:
 
@@ -12,10 +12,14 @@ class Salary:
 		state_code = rec['state_code']
 		rec ['state_name'] = STATE_CODE_MAP[state_code]
 
+# This is the Data
+
 input_data = [
 	{'employee_name': 'Susan', 'salary': 100000.0, 'state_code': 'WA'},
 	{'employee_name': 'Ellen', 'salary': 75000.0, 'state_code': 'TX'},
 	]
+
+# This is the actual script
 
 for rec in input_data:
 	Salary.update_employee_record(rec)
@@ -24,7 +28,3 @@ for rec in input_data:
 	state = rec['state_name']
 	print(name + ' now lives in ' +  state)
 	print( 'and makes $' +  str(salary))
-
-#edit to test adding a branch and make general corrections
-
-
